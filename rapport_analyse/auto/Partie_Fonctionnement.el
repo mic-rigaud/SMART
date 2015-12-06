1d54a619bba7ab3b2c4562f14cb4a736d4c7c45c
rgd <michael@mic-rigaud.fr>
2015-12-06 11:20:16 +0100

    [rangement des fichier]

---
+++ rapport_analyse/auto/Partie_Fonctionnement.el
@@ -0,0 +1,5 @@
+(TeX-add-style-hook "Partie_Fonctionnement"
+ (lambda ()
+    (LaTeX-add-labels
+     "chap:fonctionnement")))
+
---
+++ rapport_analyse/auto/Partie_Radiogonio.el
@@ -0,0 +1,5 @@
+(TeX-add-style-hook "Partie_Radiogonio"
+ (lambda ()
+    (LaTeX-add-labels
+     "Watson-Watt")))
+
